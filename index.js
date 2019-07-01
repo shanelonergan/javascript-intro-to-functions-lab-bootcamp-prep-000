33b3bed329824ba268b10da745f6a7c45f3eb2f0
Shane Lonergan <sptlonergan@gmail.com>
2019-07-01 15:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ function whisper(string) {
 }
 
 function logWhisper(string) {
-  console.log('hello')
+  console.log('hello');
 }
 
-var uppercase = "HELLO"
-var lowercase = "hello"
+var uppercase = 'HELLO';
+var lowercase = 'hello';
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === lowercase)
-    return ("I can't hear you!")
+    return ('I can\'t hear you!');
   if (string.toUpperCase === uppercase)
-    return ("YES INDEED!")
-  if (string === "I love you, Grandma")
-    return ("I love you, too")
+    return ('YES INDEED!');
+  if (string === 'I love you, Grandma')
+    return ('I love you, too');
 }
