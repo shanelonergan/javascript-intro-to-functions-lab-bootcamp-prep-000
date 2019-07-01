845a7c010f2d83cfe0c4761746e884f0ce2650c0
Shane Lonergan <sptlonergan@gmail.com>
2019-07-01 15:36:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function whisper(string) {
 function logWhisper(string) {
   console.log('hello')
 }
+
+var uppercase = "HELLO"
+var lowercase = "hello"
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === lowercase)
+    return ("I can't hear you!") 
+  if (string.toUpperCase === uppercase)
+    return ("YES INDEED!")
+  if (string === "I love you, Grandma")
+    return ("I love you, too")
+}
