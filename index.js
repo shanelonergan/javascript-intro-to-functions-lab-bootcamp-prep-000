3bd827ab558b22ed0b2d5f74346baa4dbe15da59
Shane Lonergan <sptlonergan@gmail.com>
2019-07-01 15:33:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
